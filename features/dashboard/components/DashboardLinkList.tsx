7b43d846e1782e071a87117e3501a078b13c4f76
lynj <navyoonj@gmail.com>
2026-04-02 14:02:41 -0700

    Feat: [Dashboard] Retain search params

--- features/dashboard/components/DashboardLinkList.tsx
+++ features/dashboard/components/DashboardLinkList.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 const DashboardLinkList = () => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const queryString = searchParams?.toString()
+    ? '?' + searchParams.toString()
+    : '';
   const linkClass = (path: string) =>
     pathname === path || pathname.startsWith(path + '/') ? '' : 'text-gray-300';
   const costPathActive =
@@ -12,19 +16,21 @@ const DashboardLinkList = () => {
   return (
     <>
       {/* <Link
-        href="/dashboard/revenue"
+        href={`/dashboard/revenue${queryString}`}
         className={linkClass('/dashboard/revenue')}
       >
         Revenue
       </Link> */}
       <Link
-        href="/dashboard/cost"
+        href={`/dashboard/cost${queryString}`}
         className={costPathActive ? '' : 'text-gray-300'}
       >
         Cost
       </Link>
-
-      {/* <Link href="/dashboard/labor" className={linkClass('/dashboard/labor')}>
+      {/* <Link
+        href={`/dashboard/labor${queryString}`}
+        className={linkClass('/dashboard/labor')}
+      >
         Labor
       </Link> */}
     </>
